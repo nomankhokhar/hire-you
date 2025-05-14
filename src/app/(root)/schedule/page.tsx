ad53c7ed3afaab3c79893dffcf037f7c326e8dff
nomannhohar <aliyounas084@gmail.com>
2025-05-14 14:35:39 +0500

    func: added Schedule Meeting Page is Done

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,5 +1,18 @@
-const SchedulePage = () => {
-  return <div>LOL</div>;
-};
+"use client";
 
+import LoaderUI from "@/components/LoaderUI";
+import { useRouter } from "next/navigation";
+import InterviewScheduleUI from "./InterviewScheduleUI";
+import { useUserRole } from "@/components/hooks/useUserRole";
+
+function SchedulePage() {
+  const router = useRouter();
+
+  const { isInterviewer, isLoading } = useUserRole();
+
+  if (isLoading) return <LoaderUI />;
+  if (!isInterviewer) return router.push("/");
+
+  return <InterviewScheduleUI />;
+}
 export default SchedulePage;
